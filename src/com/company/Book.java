ff965e6427767a46f0305dc72b06a7b8d0fd3d89
ericagleaton <ericagleaton@comcast.net>
2019-09-18 14:56:50 -0400

    first commit

---
+++ src/com/company/Book.java
@@ -0,0 +1,77 @@
+package com.company;
+
+public class Book {
+    private  String bookTitle, author, description;
+    private double price;
+    private boolean isInStock;
+
+
+    // Constructor runs when the keyword 'new' is encountered
+    public Book(){
+        System.out.println("The book constructor has run");
+    }
+    // Overloaded constructor allows us to pass parameters
+    public Book(String greeting,String sender){
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.description = description;
+        this.price = price;
+        this.isInStock = isInStock;
+    }
+
+    public String getDisplayText(){
+        return "Author name: " + author + " Title: " + bookTitle + "Description: " + description;
+    }
+/*
+    public String displayMessage(){
+        return sender + " says " + greeting;
+    }
+    public String getGreeting(){
+        return greeting;
+    }
+    public void setGreeting(String greeting){
+        this.greeting = greeting;
+    }
+
+*/
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return isInStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        isInStock = inStock;
+    }
+}
---
+++ src/com/company/BookApp.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import sun.font.TrueTypeFont;
+
+/*
+Create a project in IntelliJ called BookApplication.
+
+Create a Book class that allows for Book title, author, description, price and isInStock variables
+
+The Book class shall contain private member variables, a default constructor, an overloaded constructor and a method called getDisplayText() which prints the author, title and description.
+
+
+
+The application shall also contain getters and setters for each private member variable.
+
+Create another class called BookApp which contains the main method. In the main method you shall create an instance of a Book and print the Author, Title and Description.
+
+
+
+Publish your solution to GitHub and submit your GitHub url as the assignment.
+
+
+ */
+public class BookApp {
+
+    public static void main(String[] args) {
+        Book b = new Book();
+        b.setAuthor("Yusuke Sugomori");
+        b.setBookTitle("Java Deep Learning Essentials");
+        b.setDescription("This is a Java Book");
+        b.setPrice(15.00);
+        b.setInStock(true);
+        System.out.println(b.getDisplayText());
+
+
+
+        Book b2 = new Book();
+        b2.setAuthor("Edward Lavieri, Peter Verhas");
+        b2.setBookTitle("Mastering Java 9");
+        b2.setDescription("This is a Java Book about Java 9");
+        b2.setPrice(25.00);
+        b2.setInStock(true);
+        System.out.println(b2.getDisplayText());
+
+        /*   g.setSender("Dave");
+                g.setGreeting("Hello");
+                String message = g.displayMessage();
+                System.out.println(message);
+
+                Greeting g2 = new Greeting();
+                g2.setSender("Melissa");
+                g2.setGreeting("Hello from Melissa");
+                System.out.println(g2.getSender());
+                System.out.println(g2.getGreeting());
+
+                Greeting g3 = new Greeting("Jennifer","I like Java");
+                System.out.println(g3.displayMessage()
+         */
+    }
+
+
+}
